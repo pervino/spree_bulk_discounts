489cb54bf640ce029ad50d5915c3a5559075cb37
darbs <darby.perez@gmail.com>
2014-12-08 14:27:46 -0600

    update defaults

--- app/models/spree/bulk_discount_configuration.rb
+++ app/models/spree/bulk_discount_configuration.rb
@@ -2,6 +2,6 @@ module Spree
   class BulkDiscountConfiguration < Preferences::Configuration
     preference :label, :string, :default => 'Bulk Discount'
     preference :max_precent_rate, :double, :default => 0.9
-    preference :min_quantity, :double, :default => 3
+    preference :min_quantity, :integer, :default => 3
   end
 end
