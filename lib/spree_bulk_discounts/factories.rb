7353fe7d0ea257fc1f069ed549e0afbb5fd4d717
darbs <darby.perez@gmail.com>
2015-01-05 14:13:12 -0600

    factory fix

--- lib/spree_bulk_discounts/factories.rb
+++ lib/spree_bulk_discounts/factories.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
 
   factory :bulk_discount, class: Spree::BulkDiscount do
     name "tacos (bulk)"
-    label "nachos"
     discount_method "percent"
     break_points { {"6" => BigDecimal('0.1'), "12" => BigDecimal('0.15'), "18" => BigDecimal('0.2')} }
   end
