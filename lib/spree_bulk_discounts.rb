41baa1f6f49fe065b27136a56e48005b6054b314
darbs <darby.perez@gmail.com>
2015-02-26 12:15:49 -0600

    config fix

--- lib/spree_bulk_discounts.rb
+++ lib/spree_bulk_discounts.rb
@@ -1,2 +1,11 @@
 require 'spree_core'
+
+module Spree
+  module BulkDiscount
+    def self.config(&block)
+      yield(Spree::BulkDiscount::Config)
+    end
+  end
+end
+
 require 'spree_bulk_discounts/engine'
