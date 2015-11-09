2581a616849ad30e4a4484820333635803283918
Anthony D'Addeo <amdaddeo@gmail.com>
2015-11-09 12:37:12 -0600

    Fix Adjuster

--- app/models/spree/adjustable/adjuster/bulk_discount.rb
+++ app/models/spree/adjustable/adjuster/bulk_discount.rb
@@ -3,17 +3,16 @@ module Spree
     module Adjuster
       class BulkDiscount < Spree::Adjustable::Adjuster::Base
         def update
-          bulk_discount_total = adjustable.adjustments.bulk_discount.reload.map do |adjustment|
-            adjustment.update!(adjustable)
-          end.compact.sum
+          bulk_discount = adjustments.bulk_discount
+          bulk_discount_total = bulk_discount.reload.map(&:update!).compact.sum
 
           update_totals(bulk_discount_total)
         end
 
         private
 
-        def line_item?
-          @adjustable.is_a? Spree::LineIitem
+        def adjustments
+          adjustable.try(:all_adjustments) || adjustable.adjustments
         end
 
         def update_totals(bulk_discount_total)
