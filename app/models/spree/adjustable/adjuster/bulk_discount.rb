960bcddce72121cfeed33e478b763275abfe4fa5
Anthony D'Addeo <amdaddeo@gmail.com>
2015-11-12 12:02:06 -0600

    Eligilbe bulk discounts

--- app/models/spree/adjustable/adjuster/bulk_discount.rb
+++ app/models/spree/adjustable/adjuster/bulk_discount.rb
@@ -4,6 +4,11 @@ module Spree
       class BulkDiscount < Spree::Adjustable::Adjuster::Base
         def update
           bulk_discount = adjustments.bulk_discount
+
+          bulk_discount.each do |adj|
+            adj.update_column("eligible", true)
+          end
+
           bulk_discount_total = bulk_discount.reload.map(&:update!).compact.sum
 
           update_totals(bulk_discount_total)
@@ -12,7 +17,7 @@ module Spree
         private
 
         def adjustments
-          adjustable.try(:all_adjustments) || adjustable.adjustments
+          adjustable.try(:all_adjustments).try(:eligible) || adjustable.adjustments.try(:eligible)
         end
 
         def update_totals(bulk_discount_total)
